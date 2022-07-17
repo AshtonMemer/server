71b885a2c5987c9ad53711e1b1be93c4a4fa9e33
Alexander Epolite <alexander@epolite.net>
2022-07-16 23:19:16 -0700

    lowercase

--- src/TempMail.ts
+++ src/TempMail.ts
@@ -4,8 +4,13 @@ import EmailServer from "./srv/EmailServer";
 import Config from "./Config";
 import EmailStorage from "./util/EmailStorage";
 import HTTPServer from "./srv/HTTPServer";
+import Email from "./entity/Email";
 
-new EmailServer(Config.MAIL_PORT, (email) => {
+new EmailServer(Config.MAIL_PORT, (email: Email[]) => {
+    //convert the `to` field to lowercase
+    for(const e of email) {
+        e.to = e.to.toLowerCase();
+    }
     email.forEach(EmailStorage.addEmail);
 });
 
--- src/entity/Email.ts
+++ src/entity/Email.ts
@@ -17,7 +17,7 @@ export default class Email {
      */
     public constructor(
         public readonly from: string,
-        public readonly to: string,
+        public to: string,
         public readonly subject: string,
         public readonly body: string,
         public readonly date: number,
